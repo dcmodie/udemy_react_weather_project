ef06024e1be171e323787c006138c6a0c227bca1
Doug Modie <dmodie@gmail.com>
2016-07-12 18:52:51 -0500

    installed npm, added SearchBar component

---
+++ src/containers/search_bar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+class SearchBar extends Component {
+
+    render (){
+      return (
+          <div className="input-group" >
+            This is a search bar 
+
+          </div>
+
+      );
+    }
+
+
+}
+
+export default SearchBar
