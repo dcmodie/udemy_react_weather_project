5e9555eba8a0b7a4fab32e62cc97c002df5c1d6e
Doug Modie <dmodie@gmail.com>
2016-08-13 19:03:23 -0400

    removed test_containter, forecast_list over to containters dir

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import SearchBar from '../containers/search_bar.js'
-import ForcastList from './forecast_list.js'
-import TestContainer from '../containers/test_container.js'
+import ForcastList from '../containers/forecast_list.js'
 
 
 export default class App extends Component {
@@ -10,7 +9,6 @@ export default class App extends Component {
       <div>
       <SearchBar />
       <ForcastList />
-      <TestContainer />
 
       </div>
     );
--- src/containers/forecast_list.js
+++ src/containers/forecast_list.js
@@ -1,11 +1,62 @@
 import React from 'react';
-const ForcastList = (props) => {
-  return   (
-      <div className='forcast-list'>Forcast List
+import { Component } from 'react';
+import { connect } from 'react-redux';
+import {bindActionCreators} from 'redux';
+import { fetchWeather } from '../actions/index.js';
 
-      </div>
-    );
-};
+class ForcastList extends Component {
 
+  constructor(props) {
+    super (props);
+  }
+  render (){
+    console.log("render in forcast list called " )
 
-export default ForcastList;
+   console.log(this.props.forcasts)
+    if (this.props["forcasts"]){
+        return (
+          <div>
+            <h3>Details For: </h3>
+            <div>
+            {
+              this.props.forcasts.statusText
+            }
+            </div>
+            <div>
+            {
+              this.props.forcasts.data.city.name
+            }
+            </div>
+          </div>
+        );
+      }
+      else {
+        return (
+          <div>no data yet</div>
+        )
+      }
+  }
+}
+
+//export default BooksList
+// if state changes, container will re-render
+function mapStateToProps(state){
+  //whatever gets returned will show up as props inside of
+  return{
+     forcasts: state.forcastList
+   }
+}
+
+//Anything returned from this function will end up as props on the SearchBar container
+// function mapDispatchToProps (dispatch){
+//   // whenever fetchWeather is called, result should be passed
+//   // to all of our reducers
+//   return bindActionCreators({fetchWeather}, dispatch)
+// }
+
+//Promote BookList from a component to a container
+// it needs to know about this new dispatch method, selectBook.
+// make it available as a prop
+export default connect( mapStateToProps, null)(ForcastList)
+
+//export default SearchBar
